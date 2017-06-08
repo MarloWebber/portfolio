2c37d53aeff5685584b29ff942d765f9dbf5b5b8
Marlo Webber <Marlo Webber>
2017-06-08 16:10:07 +1000

    added jscoils and new web store server/db startup batch file

--- Storefront/websiteInit.bat
+++ Storefront/websiteInit.bat
@@ -3,6 +3,6 @@ cd "C:\data\db"
 
 start "MongoD" /d "C:\Program Files\MongoDB\Server\3.2\bin\" mongod.exe
 
-cd "C:\Users\Marlo\Desktop\projects incomplete\nodetutorial"
+cd "C:\Users\Marlo\Documents\My Github\portfolio/Storefront"
 
 node app.js
